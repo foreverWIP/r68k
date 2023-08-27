0f7033eb91baf44190e26b2b546e02375384cd58
Amy Wright <amywrightmail@protonmail.com>
2023-08-27 02:23:48 -0700

    update build script for non-msvc cc support

--- emu/build.rs
+++ emu/build.rs
@@ -50,7 +50,7 @@ fn main() {
         let _ = fs::remove_file(musashi_dir.join(Path::new("m68kmake.obj")));
     } else {
         match Command::new(cc_name)
-            .args(&["-o", "m68kmake.exe"])
+            .args(&["-o", "m68kmake"])
             .arg(&format!("{}", m68kmake_file_str))
             .current_dir(&musashi_dir)
             .status()
@@ -58,6 +58,24 @@ fn main() {
             Ok(_) => {}
             Err(e) => panic!("Compiling m68kmake failed!: {}", e.to_string()),
         }
+        match Command::new(musashi_dir.join(Path::new("m68kmake")))
+            .current_dir(&musashi_dir)
+            .status()
+        {
+            Ok(_) => {}
+            Err(e) => panic!("Running m68kmake failed!: {}", e.to_string()),
+        };
+        cc::Build::new()
+            .file(musashi_dir.join(Path::new("m68kcpu.c")))
+            .file(musashi_dir.join(Path::new("m68kops.c")))
+            .file(
+                musashi_dir
+                    .join(Path::new("softfloat"))
+                    .join(Path::new("softfloat.c")),
+            )
+            .compile("musashi");
+        let _ = fs::remove_file(musashi_dir.join(Path::new("m68kmake")));
+        let _ = fs::remove_file(musashi_dir.join(Path::new("m68kmake.o")));
     }
     println!("cargo:rerun-if-changed={}", musashi_dir.to_str().unwrap());
 }
